c7c1e62a63c9e47ed9c9f9f9eace3ed5e87081fd
Andrei <97633022+Zmei55@users.noreply.github.com>
2023-10-14 23:56:28 +0200

    PassEnc update

--- src/main/java/com/hospital_spring/HospitalSpringApplication.java
+++ src/main/java/com/hospital_spring/HospitalSpringApplication.java
@@ -1,11 +1,7 @@
 package com.hospital_spring;
 
-//import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
 public class HospitalSpringApplication {
