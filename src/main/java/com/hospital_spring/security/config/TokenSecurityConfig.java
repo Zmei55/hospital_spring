45083584c959a7a527516a94432419513f28da93
Andrei <97633022+Zmei55@users.noreply.github.com>
2023-10-19 10:20:43 +0200

    logoutHandler added

--- src/main/java/com/hospital_spring/security/config/TokenSecurityConfig.java
+++ src/main/java/com/hospital_spring/security/config/TokenSecurityConfig.java
@@ -1,5 +1,6 @@
 package com.hospital_spring.security.config;
 
+import com.hospital_spring.security.config.details.CustomLogoutHandler;
 import com.hospital_spring.security.config.details.UserDetailsServiceImpl;
 import com.hospital_spring.security.filter.JwtAuthenticationFilter;
 import com.hospital_spring.security.filter.JwtAuthorizationFilter;
@@ -7,7 +8,6 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.http.HttpStatus;
-import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -16,6 +16,7 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.HttpStatusEntryPoint;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.web.authentication.logout.HttpStatusReturningLogoutSuccessHandler;
 
 import static com.hospital_spring.security.filter.JwtAuthenticationFilter.*;
 
@@ -24,7 +25,7 @@ import static com.hospital_spring.security.filter.JwtAuthenticationFilter.*;
 public class TokenSecurityConfig {
     private final UserDetailsServiceImpl userDetailsServiceImpl;
     private final PasswordEncoder passwordEncoder;
-    private final AuthenticationProvider refreshAuthenticationProvider;
+    private final CustomLogoutHandler logoutHandler;
 
     @Bean
     public SecurityFilterChain securityFilterChain(
@@ -34,6 +35,7 @@ public class TokenSecurityConfig {
     ) throws Exception {
         httpSecurity.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS); // отключает сессии (куки)
         httpSecurity.csrf().disable();
+        httpSecurity.cors().disable();
 
         httpSecurity.authorizeRequests()
             .antMatchers("/swagger-ui/**").permitAll()
@@ -45,20 +47,23 @@ public class TokenSecurityConfig {
             .authenticationEntryPoint(new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED))
             .and()
             .formLogin()
-            .loginProcessingUrl(SIGN_IN_URL)
+            .loginPage(SIGN_IN_URL)
+            .permitAll()
             .and()
             .logout()
-            .logoutUrl(SIGN_OUT_URL);
+            .logoutUrl(SIGN_OUT_URL)
+            .addLogoutHandler(logoutHandler)
+            .logoutSuccessHandler(new HttpStatusReturningLogoutSuccessHandler(HttpStatus.OK))
+            .permitAll();
 
         httpSecurity.addFilter(jwtAuthenticationFilter);
-        httpSecurity.addFilterBefore(jwtAuthorizationFilter, UsernamePasswordAuthenticationFilter.class); // ложим фильтр перед юзерпасворд...(самый первый)
+        httpSecurity.addFilterBefore(jwtAuthorizationFilter, UsernamePasswordAuthenticationFilter.class); // кладёт фильтр перед UsernamePassword...(самый первый)
 
         return httpSecurity.build();
     }
 
     @Autowired
     public void bindUserDetailsServiceAndPasswordEncoder(AuthenticationManagerBuilder builder) throws Exception {
-        builder.authenticationProvider(refreshAuthenticationProvider); // refreshToken
         builder.userDetailsService(userDetailsServiceImpl).passwordEncoder(passwordEncoder); // accessToken
     }
 }
