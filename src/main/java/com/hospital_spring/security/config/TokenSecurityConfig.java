8c17fe27802bc137c7ae44977d836d4dd32ab241
Andrei <97633022+Zmei55@users.noreply.github.com>
2023-10-30 12:56:22 +0100

    TokenSecurityConfig update

--- src/main/java/com/hospital_spring/security/config/TokenSecurityConfig.java
+++ src/main/java/com/hospital_spring/security/config/TokenSecurityConfig.java
@@ -1,22 +1,28 @@
 package com.hospital_spring.security.config;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.hospital_spring.security.config.details.CustomLogoutHandler;
 import com.hospital_spring.security.config.details.UserDetailsServiceImpl;
 import com.hospital_spring.security.filter.JwtAuthenticationFilter;
 import com.hospital_spring.security.filter.JwtAuthorizationFilter;
+import com.hospital_spring.shared.dto.ResponseDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.HttpStatusEntryPoint;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.security.web.authentication.logout.HttpStatusReturningLogoutSuccessHandler;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+import javax.servlet.http.HttpServletResponse;
 
 import static com.hospital_spring.security.filter.JwtAuthenticationFilter.*;
 
@@ -26,6 +32,7 @@ public class TokenSecurityConfig {
     private final UserDetailsServiceImpl userDetailsServiceImpl;
     private final PasswordEncoder passwordEncoder;
     private final CustomLogoutHandler logoutHandler;
+    private final ObjectMapper objectMapper;
 
     @Bean
     public SecurityFilterChain securityFilterChain(
@@ -41,14 +48,21 @@ public class TokenSecurityConfig {
         httpSecurity.authorizeRequests()
             .antMatchers("/swagger-ui/**").permitAll()
             .antMatchers("/v3/api-docs/**").permitAll()
-            .antMatchers(SIGN_UP_URL).not().fullyAuthenticated() //Доступ только для не зарегистрированных пользователей
+            .antMatchers(HttpMethod.POST, SIGN_UP_URL).not().fullyAuthenticated() //Доступ только POST для не зарегистрированных пользователей
             .anyRequest().hasAuthority("SURGERY__TREATMENT_ROOM")
             .and()
             .exceptionHandling()
-            .authenticationEntryPoint(new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED))
+            .defaultAuthenticationEntryPointFor((
+                    (request, response, authException) ->
+                        fillResponse(response, HttpStatus.UNAUTHORIZED, "User unauthorized")),
+                new AntPathRequestMatcher("/api/**")
+            )
             .and()
             .formLogin()
             .loginPage(SIGN_IN_URL)
+            .failureHandler((
+                (request, response, exception) ->
+                    fillResponse(response, HttpStatus.UNAUTHORIZED, "Incorrect password or username")))
             .permitAll()
             .and()
             .logout()
@@ -65,6 +79,25 @@ public class TokenSecurityConfig {
 
     @Autowired
     public void bindUserDetailsServiceAndPasswordEncoder(AuthenticationManagerBuilder builder) throws Exception {
-        builder.userDetailsService(userDetailsServiceImpl).passwordEncoder(passwordEncoder); // accessToken
+        builder.userDetailsService(userDetailsServiceImpl).passwordEncoder(passwordEncoder); // token
+    }
+
+    // ручное заполнение ответа
+    private void fillResponse(HttpServletResponse response, HttpStatus status, String message) {
+        try {
+            response.setStatus(status.value());
+            response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+
+            ResponseDto responseDto = ResponseDto.builder()
+                .status(status.value())
+                .message(message)
+                .build();
+
+            String body = objectMapper.writeValueAsString(responseDto);
+
+            response.getWriter().write(body);
+        } catch (Exception e) {
+            throw new IllegalStateException(e);
+        }
     }
 }
